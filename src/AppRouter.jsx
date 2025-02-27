8601dc032b52bf0ab9bc13e1cd7e120093bfb5ec
asshankhapal <shankhapalakash@gmail.com>
2025-02-27 22:09:38 +0530

    Akash

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import React from "react";
 import HomePage from "./pages/home";
 import RootLayout from "./layouts/RootLayout";
+import RoadmapComponent from "./pages/dashboard/Roadmap/Roadmappage";
+import VideoPage from "./pages/dashboard/video/videopage";
+import Messages from "./pages/message/Messages";
 
 const AppRouter = () => {
   return (
@@ -9,6 +12,9 @@ const AppRouter = () => {
       <Routes>
         <Route element={<RootLayout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/roadmap" element={<RoadmapComponent />} />
+          <Route path="/video" element={<VideoPage />} />
+          <Route path="/message" element={<Messages />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,7 +5,7 @@ const RootLayout = () => {
   return (
     <div className="w-full h-full">
       <RootNav />
-      <main className="px-10 sm:px-48 xl:px-48">
+      <main className="">
         <Outlet />
       </main>
     </div>
