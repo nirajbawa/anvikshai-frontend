405ded65bd0039bb1d7725e724587f7484c9d8dd
PranjalWagh <pranjalwagh2005@gamail.com>
2025-02-28 22:49:03 +0530

    PranjalWagh

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import React from "react";
 import HomePage from "./pages/home";
 import RootLayout from "./layouts/RootLayout";
+import Assignment from "./pages/AssignmentComp/Assignment";  // Import the new page
+import Article from "./pages/ArticleComp/Article";
+import Quiz from "./pages/QuizComp/Quiz";
 
 const AppRouter = () => {
   return (
@@ -9,6 +12,9 @@ const AppRouter = () => {
       <Routes>
         <Route element={<RootLayout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/assignment" element={<Assignment />} /> {/* New Route */}
+          <Route path="/article" element={<Article />} /> {/* New Route */}
+          <Route path="/quiz" element={<Quiz />} /> {/* New Route */}
         </Route>
       </Routes>
     </BrowserRouter>
