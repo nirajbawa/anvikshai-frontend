f1d2f45b53ec6c7111eeea83f000986721ab192d
unknown <vaishnavineha13@gmail.com>
2025-02-28 22:27:50 +0530

    Initial commit

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import React from "react";
-import HomePage from "./pages/home";
-import RootLayout from "./layouts/RootLayout";
-
+import DetailsPage from "./pages/home/Details";
+import PaymentPage from "./pages/home/PaymentPage";
+import Subscription from "./pages/home/Subscription";
+import RoadMap from "./pages/home/RoadMap";
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<RootLayout />}>
-          <Route path="/" element={<HomePage />} />
-        </Route>
+        <Route path="/details" exact element={<DetailsPage />} />
+        {/* Add more routes as needed */}
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/subscription" element={<Subscription />} />
+        <Route path="/roadmap" element={<RoadMap />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/navbars/RootNav.jsx
+++ src/components/navbars/RootNav.jsx
@@ -1,112 +1,30 @@
-import * as React from "react";
-import {
-  Button,
-  IconButton,
-  Typography,
-  Collapse,
-  Navbar,
-} from "@material-tailwind/react";
-import {
-  Archive,
-  Menu,
-  MultiplePages,
-  ProfileCircle,
-  SelectFace3d,
-  Xmark,
-  MediaImage,
-} from "iconoir-react";
-
-const LINKS = [
-  {
-    icon: MultiplePages,
-    title: "Pages",
-    href: "#",
-  },
-  {
-    icon: ProfileCircle,
-    title: "Account",
-    href: "#",
-  },
-  {
-    icon: SelectFace3d,
-    title: "Blocks",
-    href: "#",
-  },
-  {
-    icon: Archive,
-    title: "Docs",
-    href: "#",
-  },
-];
-
-function NavList() {
+function RootNav() {
   return (
-    <ul className="mt-4 flex flex-col gap-x-3 gap-y-1.5 lg:mt-0 lg:flex-row lg:items-center">
-      {LINKS.map(({ icon: Icon, title, href }) => (
-        <li key={title}>
-          <Typography
-            as="a"
-            href={href}
-            type="small"
-            className="flex items-center gap-x-2 p-1 hover:text-primary"
-          >
-            <Icon className="h-4 w-4" />
-            {title}
-          </Typography>
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-export default function RootNav() {
-  const [openNav, setOpenNav] = React.useState(false);
-
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false)
-    );
-  }, []);
-
-  return (
-    <Navbar className="sticky top-0 mx-auto w-full ">
-      <div className="flex items-center">
-        <Typography
-          as="a"
-          href="#"
-          type="small"
-          className="ml-2 mr-2 block py-1 font-semibold"
-        >
-          Material Tailwind
-        </Typography>
-        <hr className="ml-1 mr-1.5 hidden h-5 w-px border-l border-t-0 border-secondary-dark lg:block" />
-        <div className="hidden lg:block">
-          <NavList />
+    <div>
+      <nav class="sticky bg-[#D2B0FD] h-18 w-full shadow-lg ">
+        <div class="flex justify-between items-center p-3 space-x-11">
+          <div class=" font-bold ml-8 flex flex-1/2 text-[30px]">AnvikshAI</div>
+          <div>
+            <button
+              class="cursor-pointer rounded-md hover:active
+                border-1 p-2.5 px-6 active:bg-black
+                active:text-white
+                active:border-black"
+            >
+              Login
+            </button>
+            <button
+              class=" cursor-pointer mr-10 bg-black text-white p-3 rounded-md px-5 
+                hover:active
+                active:bg-[#3b3939]"
+            >
+              Signup
+            </button>
+          </div>
         </div>
-        <Button size="sm" className="hidden lg:ml-auto lg:inline-block">
-          Sign In
-        </Button>
-        <IconButton
-          size="sm"
-          variant="ghost"
-          color="secondary"
-          onClick={() => setOpenNav(!openNav)}
-          className="ml-auto grid lg:hidden"
-        >
-          {openNav ? (
-            <Xmark className="h-4 w-4" />
-          ) : (
-            <Menu className="h-4 w-4" />
-          )}
-        </IconButton>
-      </div>
-      <Collapse open={openNav}>
-        <NavList />
-        <Button isFullWidth size="sm" className="mt-4">
-          Sign In
-        </Button>
-      </Collapse>
-    </Navbar>
+      </nav>
+    </div>
   );
 }
+
+export default RootNav;
